f336abf3f5db50861d554043d7907103128741e3
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2021-12-13 02:20:28 +0530

    BASIC

---
+++ BASIC/Cyclically rotate an array by one.py
@@ -0,0 +1,68 @@
+#QUESTION
+"""
+Given an array, rotate the array by one position in clock-wise direction.
+ 
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1, 2, 3, 4, 5}
+Output:
+5 1 2 3 4
+ 
+
+Example 2:
+
+Input:
+N = 8
+A[] = {9, 8, 7, 6, 4, 2, 1, 3}
+Output:
+3 9 8 7 6 4 2 1
+ 
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function rotate() which takes the array A[] and its size N as inputs and modify the array.
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+"""
+#SOLUTION
+#User function Template for python3
+
+def rotate( arr, n):
+    
+    t= arr.pop()
+    arr.insert(0,t)
+    
+    return arr
+    
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        rotate(a, n)
+        print(*a)
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
